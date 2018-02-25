df3ac823f104bccd529fff01b0eb621ebe0611d7
Neyko-qt <fauster08@mail.ru>
2018-02-26 01:21:11 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.comparator = function (a, b) {
+      return a - b;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element)
   }
 
   at(index) {
-    // your implementation
+    return this.array[index]
   }
 
   get length() {
-    // your implementation
+    return this.array.length
   }
 
   toArray() {
-    // your implementation
+    return this.array
   }
 
   sort(indices) {
-    // your implementation
+    var unsortedArray = []
+      for (var i = 0; i < indices.length; i++){
+          unsortedArray.push(this.array[indices[i]]);
+      }
+
+      unsortedArray.sort(this.comparator)
+
+      for (var j = 0; j < indices.length; j++){
+          this.array[indices[j]] = unsortedArray[j];
+      }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
